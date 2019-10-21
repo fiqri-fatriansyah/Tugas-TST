aafecc9d3cec80987dd067a746eb7ce2e978ca3e
Fiqri Fatriansyah <fiqri.fatriansyah.2305@gmail.com>
2019-10-21 23:02:58 +0700

    Version 0.0.2
    Added more features

--- Tugas API/list.py
+++ Tugas API/list.py
@@ -1,18 +1,20 @@
+from flask import make_response, abort
+
 BOARD_GAME = {
-    "Talisman": {
-        "id": 1,
+    "0": {
+        "id": "0",
         "name": "Talisman",
         "publisher": "Asmodus Inc.",
         "quantity" : 1
     },
-    "Portal": {
-        "id": 2,
+    "1": {
+        "id": "1",
         "name": "Portal",
         "publisher": "Valve Inc.",
         "quantity" : 5
     },
-    "Portal 2": {
-        "id": 3,
+    "2": {
+        "id": "2",
         "name": "Portal 2",
         "publisher": "Valve Inc.",
         "quantity" : 2
@@ -20,5 +22,15 @@ BOARD_GAME = {
 }
 
 def read():
-    # Create the list of people from our data
+    # Create the list of board game from our data
     return [BOARD_GAME[key] for key in sorted(BOARD_GAME.keys())]
+
+def read_one(id):
+    # Create a single board game from our data
+    if id in BOARD_GAME:
+        boardgame_temp = BOARD_GAME.get(id)
+    else:
+        abort(
+            404, "Board game with id {id} not found".format(id=id)
+        )
+    return boardgame_temp
